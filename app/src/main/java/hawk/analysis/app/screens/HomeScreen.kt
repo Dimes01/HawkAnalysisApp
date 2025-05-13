55778c9ff28bf223d6db90bd5d1c72bff4978bc9
Dimes01 <91180844+Dimes01@users.noreply.github.com>
2025-05-13 11:12:15 +0400

    feat: добавлены отображение валют

--- app/src/main/java/hawk/analysis/app/screens/HomeScreen.kt
+++ app/src/main/java/hawk/analysis/app/screens/HomeScreen.kt
@@ -3,6 +3,7 @@ package hawk.analysis.app.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,17 +13,21 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import hawk.analysis.app.R
 import hawk.analysis.app.ui.components.CommonInformation
 import hawk.analysis.app.ui.components.Header
 import hawk.analysis.app.ui.theme.HawkAnalysisAppTheme
 import hawk.analysis.app.utilities.accountTI
+import hawk.analysis.app.utilities.cuurencies
 import hawk.analysis.app.utilities.state
 import hawk.analysis.app.utilities.timeFormat
 import hawk.analysis.app.viewmodels.HomeViewModel
@@ -52,6 +57,7 @@ fun Home(
     onPrevAccount: () -> Unit,
     onNextAccount: () -> Unit,
 ) {
+    val defaultCurrency = R.string.currency_rub
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -91,9 +97,43 @@ fun Home(
                 modifier = Modifier.padding(vertical = 5.dp),
                 verticalArrangement = Arrangement.spacedBy(10.dp)
             ) {
+                // TODO: использовать что-то наподобие MoneyState, включающего Currency и информацию из Portfolio
                 Section("Деньги") {
-                    items(state.money) {
-
+                    items(state.money) { money ->
+                        Row(
+                            modifier = Modifier.fillMaxWidth().padding(10.dp),
+                            horizontalArrangement = Arrangement.spacedBy(10.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Row {
+                                Column(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    verticalArrangement = Arrangement.spacedBy(2.dp)
+                                ) {
+                                    Text(
+                                        text = money.name,
+                                        style = MaterialTheme.typography.headlineMedium,
+                                        color = MaterialTheme.colorScheme.onSurface
+                                    )
+                                    Text(
+                                        text = money.name,
+                                        style = MaterialTheme.typography.headlineSmall,
+                                        color = MaterialTheme.colorScheme.secondary
+                                    )
+                                }
+                                Text(
+                                    text = "",
+                                    style = MaterialTheme.typography.headlineMedium,
+                                    color = MaterialTheme.colorScheme.tertiary
+                                )
+                            }
+                            VerticalDivider(color = MaterialTheme.colorScheme.outlineVariant)
+                            Text(
+                                text = stringResource(cuurencies.getOrDefault(money.currency, defaultCurrency)),
+                                style = MaterialTheme.typography.titleMedium,
+                                color = MaterialTheme.colorScheme.onSurface,
+                            )
+                        }
                     }
                 }
             }
--- app/src/main/java/hawk/analysis/app/utilities/Formatters.kt
+++ app/src/main/java/hawk/analysis/app/utilities/Formatters.kt
@@ -1,6 +1,12 @@
 package hawk.analysis.app.utilities
 
+import androidx.compose.ui.res.stringResource
+import hawk.analysis.app.R
 import kotlinx.datetime.format.DateTimeComponents
 import kotlinx.datetime.format.char
 
 val timeFormat = DateTimeComponents.Format { hour(); char(':'); minute(); char(':'); second() }
+
+val cuurencies = HashMap<String, Int>().apply {
+    this["rub"] = R.string.currency_rub
+}
